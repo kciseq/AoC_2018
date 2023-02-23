09f19daa11c7cfcb1828850b7f348e281778e401
Krystian Cisek <k.ciseq@gmail.com>
2023-02-23 21:09:18 +0100

    midwork

--- 01_2018/main.cc
+++ 01_2018/main.cc
@@ -6,17 +6,36 @@ using namespace std;
 
 int main ()
 {
+    unsigned int i = 0;
+    long frequencies[996] = {};
+    long deviation_list[996] = {};
     ifstream input_file("input.txt");
-    int i = 0;
     long frequency_deviation = 0;
     int input_list;
     string line;
-    while(getline(input_file, line)){
-        input_list = stoi(line);
-        cout<<input_list<< endl;
+    uint8_t flag = 0;
+    while(input_file >> input_list){
         frequency_deviation += input_list;
-        i++;    
+        frequencies[i] = frequency_deviation;
+        deviation_list[i] = input_list;
+        i++;
     }
     cout<< "Frequency deviation equals to: "<<frequency_deviation<<endl;
+    for(unsigned int i = 1; i < 996; i++){
+        for(unsigned int j = i+3; j < 996; j++){
+            if((deviation_list[i] == deviation_list[j]) && (deviation_list[i-1] == deviation_list[j-1]) && (deviation_list[i+1] == deviation_list[j+1])){
+                cout<< "Indices of pattern edges are " << i << " " << j << endl;
+                flag = 1;
+                //break;
+            }
+
+        }
+        if (flag == 1)
+        {
+            //break;
+        }
+        
+    }
+    
     return 0;
 }
