3c408dd6cdc7b503213b9a50d1247e60487292a2
Krystian Cisek <k.ciseq@gmail.com>
2023-03-02 21:47:53 +0100

    first day uses vector

--- 01_2018/main.cc
+++ 01_2018/main.cc
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main ()
 {
-    unsigned int i = 0;
-    
-    int deviation_list[996] = {};
-    long frequencies[200*996] = {0};
-    int results[200*996] = {0};
+    vector<int> deviation_list;
+    vector<long> frequencies;
+    vector<int> results;
     ifstream input_file("input.txt");
     int frequency_deviation = 0;
     int input_list;
     string line;
-    uint8_t flag = 0;
+    
     unsigned int counter;
     while(input_file >> input_list){
         frequency_deviation += input_list;
-        frequencies[i] = frequency_deviation;
-        deviation_list[i] = input_list;
-        i++;
+        frequencies.push_back(frequency_deviation);
+        deviation_list.push_back(input_list);
     }
     cout<< "Frequency deviation equals to: "<<frequency_deviation<<endl;
 
     for(unsigned int i = 0; i < 199; i++){
         for(unsigned int j = 0; j<996; j++){
             frequency_deviation +=  deviation_list[j];
-            frequencies[996+(i*996)+j] = frequency_deviation;
+            frequencies.push_back(frequency_deviation);
         }
     }
 
     for(unsigned int i = 0; i < 200 * 996; i++){
         for(unsigned int j = i+1; j < 200 * 996; j++){
             if(frequencies[i] == frequencies[j]){
-                results[counter] = j;
+                results.push_back(j);
                 counter++;
             }
         }
